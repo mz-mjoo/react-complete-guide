c7b4923461f15b553c2bb451ce63b5222e7c14b5
mz-mjoo <81146603+mz-mjoo@users.noreply.github.com>
2021-11-23 17:02:59 +0900

    update react-state

---
+++ src/NewExpense/ExpenseForm.js
@@ -0,0 +1,72 @@
+import React,{useState} from 'react';
+
+import './ExpenseForm.css'
+
+const ExpenseForm = () => {
+    const [enteredTitle,setEnteredTitle] = useState('')
+    const [enteredAmount,setEnteredAmount] = useState('')
+    const [enteredDate,setEnteredDate] = useState('')
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmout : '',
+    //     enteredDate:'',
+    // })
+
+    const titleChangeHandler = (e) => {
+        // setUserInput((prevState)=> {
+        //     return {
+        //         ...prevState,
+        //         enteredTitle : e.target.value
+        //     }
+        // })
+        setEnteredTitle(e.target.value)
+    }
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value)
+
+    }
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value)
+
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            data:new Date(enteredDate)
+        }
+
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+
+        console.log('submit',expenseData);
+    }
+
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
+                <div className="new-expense__actions">
+                    <button type="submit">Add Expense</button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
